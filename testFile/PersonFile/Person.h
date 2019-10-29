c5e21f7baabd66a99027b97d23c6843d7d609e2a
liuyang <ayangcool100@163.com>
2019-10-29 13:46:40 +0800

    init

---
+++ testFile/PersonFile/Person.h
@@ -0,0 +1,19 @@
+//
+//  Person.h
+//  LeoXcodeProjDemo
+//
+//  Created by leo on 2019/10/29.
+//  Copyright © 2019 leo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+
++ (void)eat;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ testFile/PersonFile/Person.m
@@ -0,0 +1,17 @@
+//
+//  Person.m
+//  LeoXcodeProjDemo
+//
+//  Created by leo on 2019/10/29.
+//  Copyright © 2019 leo. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
++ (void)eat {
+    NSLog(@"eat");
+}
+
+@end
